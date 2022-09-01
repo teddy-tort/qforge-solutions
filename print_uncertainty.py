6816606f6d0e4fef627e1e09be2224d233ab80c8
Teddy Tort <etc.tortorici@gmail.com>
2022-09-01 07:36:07 -0600

    Update print_uncertainty.py

--- print_uncertainty.py
+++ print_uncertainty.py
@@ -21,51 +21,46 @@ def print_unc(value: float, uncertainty: float, units: str = None, sci_not: bool
         superscript = ["\u2070", "\u00B9", "\u00B2", "\u00B3", "\u2074",
                        "\u2075", "\u2076", "\u2077", "\u2078", "\u2079"]
         divide_by = 10 ** int(np.floor(np.log10(value)))
-        val_sci_not = value / divide_by
-        val_sci_not_order = tuple(str(int(np.log10(value / val_sci_not))))
-        unc_sci_not = uncertainty / divide_by
-        unc_sci_not_sci_not = unc_sci_not / 10 ** int(np.floor(np.log10(unc_sci_not)))
-        unc_sci_not_sci_not_order = int(np.log10(unc_sci_not / unc_sci_not_sci_not))
-        if unc_sci_not_sci_not_order < 0:
-            sig_fig = abs(unc_sci_not_sci_not_order)
-            if unc_sci_not_sci_not < 2:
-                sig_fig += 1
-            print_string = f"({val_sci_not:.{sig_fig}f} {pm:s} {unc_sci_not:.{sig_fig}f}) * 10"
-            for digit in val_sci_not_order:
-                if digit == "-":
-                    print_string += "\u207B"
-                else:
-                    print_string += superscript[int(digit)]
+        value_sci_not = value / divide_by
+        val_sci_not_order = tuple(str(int(np.log10(value / value_sci_not))))
+        value = value_sci_not
+        uncertainty /= divide_by
+    # put uncertainty into scientific notation -> uncertainty = unc_sci_not * 10^unc_sci_not_order
+    unc_sci_not = uncertainty / 10 ** int(np.floor(np.log10(uncertainty)))
+    unc_sci_not_order = int(np.log10(uncertainty / unc_sci_not))
+    if unc_sci_not_order < 0:  # if the uncertainty is a decimal, we have to treat the print a particular way.
+        # the number of sig-figs will match the order of magnitude of the uncertainty
+        sig_fig = abs(unc_sci_not_order)
+        if unc_sci_not < 2:  # except if the first digit is 1, then we have one extra sig-fig
+            sig_fig += 1
+        print_string = f"{value:.{sig_fig}f} {pm:s} {uncertainty:.{sig_fig}f}"
+
+    elif uncertainty < 2:  # if uncertainty is 1.x, the we need to print 1 sig fig passed the decimal
+        print_string = f"{value:.1f} {pm:s} {uncertainty:.1f}"
+
+    else:  # if uncertainty is a whole number, we have to truncate somewhere on the left side of the decimal
+        if unc_sci_not < 2:  # if uncertainty starts with 1.
+            value = int(np.round(value * 10 ** -(unc_sci_not_order - 1)) * 10 ** (unc_sci_not_order - 1))
+            uncertainty = int(np.round(unc_sci_not * 10) * 10 ** (unc_sci_not_order - 1))
+        else:
+            value = int(np.round(value * 10 ** -unc_sci_not_order) * 10 ** unc_sci_not_order)
+            uncertainty = int(np.round(unc_sci_not) * 10 ** unc_sci_not_order)
+        print_string = f"{value:d} {pm:s} {uncertainty:d}"
+
+    if sci_not:
+        print_string = f"({print_string:s}) * 10"
+        for digit in val_sci_not_order:
+            if digit == "-":
+                print_string += "\u207B"
+            else:
+                print_string += superscript[int(digit)]
         if units is not None:
             print_string += " " + units
-
-    else:
-        # put uncertainty into scientific notation -> uncertainty = unc_sci_not * 10^unc_sci_not_order
-        unc_sci_not = uncertainty / 10 ** int(np.floor(np.log10(uncertainty)))
-        unc_sci_not_order = int(np.log10(uncertainty / unc_sci_not))
-        if unc_sci_not_order < 0:  # if the uncertainty is a decimal, we have to treat the print a particular way.
-            # the number of sig-figs will match the order of magnitude of the uncertainty
-            sig_fig = abs(unc_sci_not_order)
-            if unc_sci_not < 2:  # except if the first digit is 1, then we have one extra sig-fig
-                sig_fig += 1
-            print_string = f"{value:.{sig_fig}f} {pm:s} {uncertainty:.{sig_fig}f}"
-
-        elif uncertainty < 2:  # if uncertainty is 1.x, the we need to print 1 sig fig passed the decimal
-            print_string = f"{value:.1f} {pm:s} {uncertainty:.1f}"
-
-        else:  # if uncertainty is a whole number, we have to truncate somewhere on the left side of the decimal
-            if unc_sci_not < 2:  # if uncertainty starts with 1.
-                value = int(np.round(value * 10 ** -(unc_sci_not_order - 1)) * 10 ** (unc_sci_not_order - 1))
-                uncertainty = int(np.round(unc_sci_not * 10) * 10 ** (unc_sci_not_order - 1))
-            else:
-                value = int(np.round(value * 10 ** -unc_sci_not_order) * 10 ** unc_sci_not_order)
-                uncertainty = int(np.round(unc_sci_not) * 10 ** unc_sci_not_order)
-            print_string = f"{value:d} {pm:s} {uncertainty:d}"
-        if units is not None:
-            print_string = f"({print_string:s}) {units:s}"
+    elif units is not None:
+        print_string = f"({print_string:s}) {units:s}"
     print(print_string)
     return print_string
 
 
 if __name__ == "__main__":
-    print_unc(0.001, 0.000004, units="m", sci_not=True)
+    print_unc(0.001, 0.0001, units="m", sci_not=True)
